4166e89e3102df75b18bc885e539d6bcb2ee899c
unknown <jjhernandez94@yahoo.com>
2018-02-07 14:53:35 -0800

    Added comments

--- app_joel.js
+++ app_joel.js
@@ -1,74 +1,28 @@
 $(document).ready(function() {
-console.log("document loaded");
- ///////////////////////////////////////////////////////////////////////////////   
-    //Firebase login configuration
-    //var mykey = config.MY_KEY;
-    // var config = {
-    // //add this to config file
-    // apiKey: "AIzaSyAhZfs5SlwDWodFF3u5Y39BOHEb893u6Ts",
-    // authDomain: "first-project-d4b8b.firebaseapp.com",
-    // databaseURL: "https://first-project-d4b8b.firebaseio.com",
-    // projectId: "first-project-d4b8b",
-    // storageBucket: "first-project-d4b8b.appspot.com",
-    // //add this to config file
-    // messagingSenderId: "1099035937686"
-    // };
 
-    //Initialize the db instance
-    // firebase.initializeApp(config);
-
-    //Variable use to reference the database.
-    // var database = firebase.database();
-
-    // connectionsRef references a specific location in our database.
-    // All of our connections will be stored in this directory.
-    // var connectionsRef = database.ref("characterCount");
-
-    // var dummyData = {
-    //     name: "Spiderman",
-    //     count: 0
-    // };
-
-    // connectionsRef.push(dummyData);
-
-    // '.info/connected' is a special location provided by Firebase that is updated every time
-    // the client's connection state changes.
-    // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
-    // var connectedRef = database.ref(".info/connected");
-    
-    // function pushTo(charName) {
-    //     firebase.database().ref().push({
-    //     name: charName,
-    //     count: characterCount++
-    //   });
-    // };
-
-///////////////////////////////////////////////////////////////////////////////
     function modal() {
         $('#myModal').modal('show');
     }
 
-    
+    //API key
+    var mykey = "feec332cd3344e9226a7105dd29cc5dae84c154d";
+
+    var proxy = 'https://cors-anywhere.herokuapp.com/';
 
     //Clicking on the submit button triggers the following... 
     $(document).on("click", "#inputButton", function() {
         
         //Prevents the document from reloading
         event.preventDefault();
-
-        //Grab the API key from config
-        var mykey = config.MY_KEY;
+        
         //Store the user's string into comicName
         var comicName = $("#characterInput").val().trim();
+        
+        //Delete the string the user entered
         $("#characterInput").val("");
-        // pushTo(charName);
 
-        $("#characterInput").text("");
-
+        //The URL to be entered into the AJAX call
         var queryURL = "https://comicvine.gamespot.com/api/characters/?api_key=" + mykey + "&filter=name:" + comicName + "&limit=100&format=json";
-            console.log(queryURL);
-
-        var proxy = 'https://cors-anywhere.herokuapp.com/';
             
         //Boolean variable for character matching
         var charFound = false;
@@ -82,10 +36,11 @@ console.log("document loaded");
           method: 'GET'
         }).done(function(response) {
 
+            //This For loop goes through the array returned, targets the object property, name and checks if it matches with the user input string  
             for (var i = 0; i < response.results.length; i++) {
                 
                 var character = response.results[i];
-                console.log(response.results[i]);
+            
                 //Targets character key
                 var characterName = character.name; 
                 
@@ -103,17 +58,19 @@ console.log("document loaded");
                 
                 //Lower cases the user input string
                 var lcComicName = comicName.toLowerCase();
-                console.log(lcComicName);
                 
                 //Assigns the response id value to originId
                 var originId = response.results[i].id;
 
+                //Checks current index name string matches with the user input string
                 if (lcCharacterName === lcComicName) {     
+                    //If they match, set charFound to true and break the loop
                     charFound = true;
                     break;
                 }
             }
             
+            //User input validation: if the user entry is not Marvel, display modal
             if (charFound) {
                  if (publisherName === "Marvel") {
                     isMarvel = true;
@@ -122,73 +79,46 @@ console.log("document loaded");
                    modal();
                 }
             }
-            // console.log(characterImage);
+
+            //Once character is confirmed to be a Marvel character, display name, real name, description and image to DOC
             if (isMarvel) {
                 $("#character_bio").html("<h5>Character Name: " + characterName + "</h5>" + 
-                    "<h5>Character Real Name: " + character.real_name + "</h5><h5>Birth Date: " + character.birth + 
+                    "<h5>Character Real Name: " + character.real_name + 
                     "</h5>" + "<h5>Character Bio and Synopsis:</h5>" + "<p>" + character.description + "</p>");
                 $(".slide1").html("<img src=" + characterImage + " height=" + 500 + " width=" + 806.25 + "></img>");
-            }
-        });
-    });
-});
+            }//Closing if loop
+        });//Closing promise
+    });//Closing on click button function
 
+//When the element is clicked, display name, real name, description and image to DOC
 $(document).on("click", ".character", function() {
-   // In this case, the "this" keyword refers to the character that was clicked
-    //Grab the API key from config
-    var mykey2 = config.MY_KEY;
-    var proxy2 = 'https://cors-anywhere.herokuapp.com/';
+    
+    //Take the value of the element clicked and assign it to characterVal
     var characterVal = $(this).attr("data-name");
-    console.log($(this).attr("data-name"));
-    var queryURL2 = "https://comicvine.gamespot.com/api/characters/?api_key=" + mykey2 + "&filter=name:" + characterVal + "&limit=100&format=json";
-    console.log(queryURL2);
-$.ajax({
-          url: proxy2 + queryURL2,
+    
+    //Insert the API key and element value(characterVal) in to the Comic Vine API URL
+    var queryURL2 = "https://comicvine.gamespot.com/api/characters/?api_key=" + mykey + "&filter=name:" + characterVal + "&limit=100&format=json";
+
+//Initiate AJAX call
+$.ajax({  //CORS 
+          url: proxy + queryURL2,
           method: 'GET'
         }).done(function(response) {
-            console.log(response);
-            // for (var i = 0; i < response.results.length; i++) {
-                // var comicName2 = "";
-
-                // var charFound2 = false;
+                
+                //Assign the first target in the array to character2       
                 var character2 = response.results[0];
-                // console.log(response.results[0]);
+        
                 //Targets character key
                 var characterName2 = character2.name; 
                 
                 //Once a character is matched with the database, grab the
                 var characterImage2 = character2.image.original_url;
                 
-                //Lower cases the returned string 
-                // var lcCharacterName2 = characterName2.toLowerCase();
-                
-                //Lower cases the user input string
-                // var lcComicName2 = comicName2.toLowerCase();
-                // console.log(lcComicName2);
-
-                // if (lcCharacterName2 === lcComicName2) {     
-                //     charFound2 = true;
-                //     break;
-                // }
-            // }
-
-            // if (charFound2) {
+                //Display name, real name, description and image to DOC
                 $("#character_bio").html("<h5>Character Name: " + characterName2 + "</h5>" + 
-                "<h5>Character Real Name: " + character2.real_name + "</h5><h5>Birth Date: " + character2.birth + 
+                "<h5>Character Real Name: " + character2.real_name +
                 "</h5>" + "<h5>Character Bio and Synopsis:</h5>" + "<p>" + character2.description + "</p>");
-                $(".slide1").html("<img src=" + characterImage2 + " height=" + 500 + " width=" + 806.25 + "></img>");
-            // }  
-        });      
+                $(".slide1").html("<img src=" + characterImage2 + " height=" + 500 + " width=" + 806.25 + "></img>"); 
+        });//Closing promise      
     });//Closing brace and parenthasis
-// }
-//ALTERNATIVE ALIAS CODE
-// console.log(lcComicName);
-            // var characterAlias = response.results[i].aliases;    
-
-            // var lcCharacterAlias = characterAlias.toLowerCase();
-
-            // var enter = '\n';
-
-            // var arrayOfAlias = lcCharacterAlias.split(enter);
-            
-            // var index = arrayOfAlias.indexOf(comicName);
+});//Closing document ready
